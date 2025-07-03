920319ae3e989d70bcb866d0f60c33851501e790
AngelGabox <agabrieldelaroca@gmail.com>
2025-07-03 15:10:18 -0500

    errores

--- sullivan_django/urls.py
+++ sullivan_django/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -34,8 +34,12 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Endpoints de JWT para obtener y refrescar el token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     # Rutas API por módulo
+    path('admin/', admin.site.urls),
     path('estudiantes/', include('estudiantes.urls')),
     path('personas/', include('personas.urls')),
     path('cursos/', include('cursos.urls')),
@@ -47,3 +51,4 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('usuarios/', include('usuarios.urls')),
 ]
+
